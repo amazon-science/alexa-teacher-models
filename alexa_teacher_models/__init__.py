7dc21297f0dc6bfa10a3cb963bb97499d4f43dec
Daniel Pressel <dpressel@gmail.com>
2023-04-04 22:05:16 -0400

    Add version and init for scripts bump version

--- alexa_teacher_models/__init__.py
+++ alexa_teacher_models/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+__version__ = "1.0.1"
 
 from .configuration_atm import (
     AlexaTMConfig,
